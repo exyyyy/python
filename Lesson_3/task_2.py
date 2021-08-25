408b26b525bc975fa4116e356f28a7ce9a45416e
exyyyy <exyyy89@gmail.com>
2021-08-26 00:10:41 +0300

    Task 2

---
+++ Lesson_3/task_2.py
@@ -0,0 +1,10 @@
+"""def user_data(name, surname, birth_year, living_city, email, phone_number):"""
+def print_user_data(**kwargs):
+    print("Словарь:")
+    print(kwargs)
+    print()
+    print("Строка:")
+    print(" ".join(str(e) for e in list(kwargs.values())))
+    return
+
+print_user_data(name = "Алексей", surname = "Михеев", birth_year = 1989, living_city = "Kazan", email = "noname@gmail", phone_number = "+79000000000")
