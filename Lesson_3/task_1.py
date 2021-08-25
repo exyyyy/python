873263d915399957edcd556b15a7ca753375a6a9
exyyyy <exyyy89@gmail.com>
2021-08-25 22:25:55 +0300

    Task 1

---
+++ Lesson_3/task_1.py
@@ -0,0 +1,8 @@
+def my_division(var_1 = float(input("Введите делимое: ")), var_2 = float(input("Введите делитель: "))):
+    try:
+        result = var_1 / var_2
+        return result
+    except ZeroDivisionError:
+        return print("На ноль делить нельзя!")
+
+print(my_division())
