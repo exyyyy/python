ec5c902396f06bfe06f2aceb948abd9a5f2eca65
exyyyy <exyyy89@gmail.com>
2021-08-26 00:29:24 +0300

    Task 2

--- Lesson_3/task_2.py
+++ Lesson_3/task_2.py
@@ -7,4 +7,5 @@ def print_user_data(**kwargs):
     print(" ".join(str(e) for e in list(kwargs.values())))
     return
 
+
 print_user_data(name = "Алексей", surname = "Михеев", birth_year = 1989, living_city = "Kazan", email = "noname@gmail", phone_number = "+79000000000")
